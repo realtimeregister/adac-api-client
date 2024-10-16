11cf22c4f42ce7128075ba49afa0290514e9560b
kevinkosterr <koster.k2001@gmail.com>
2024-10-16 12:56:08 +0200

    refactor: sessionID in sessionStorage, use crypto.randomUUID
    - Use session storage for storing the sessionID;
    - Use crypto.randomUUID() whenever possible, otherwise fallback.

--- src/utils.ts
+++ src/utils.ts
@@ -20,6 +20,9 @@ export function onDomReady(fn: () => any) {
  * Generate a UUID
  */
 export function generateUUID(): string {
+    // @ts-expect-error randomUUID will still not exist in certain situations.
+    if (window.crypto && window.crypto.randomUUID) return crypto.randomUUID()
+
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
         const r = Math.random() * 16 | 0
         const v = c === 'x' ? r : (r & 0x3 | 0x8)
@@ -31,7 +34,7 @@ export function generateUUID(): string {
  * Ensure a session ID is set for the current user.
  */
 export function ensureSessionID(): void {
-    if (localStorage.getItem('sessionId') === null) {
-        localStorage.setItem('sessionId', generateUUID())
+    if (sessionStorage.getItem('sessionId') === null) {
+        sessionStorage.setItem('sessionId', generateUUID())
     }
 }
