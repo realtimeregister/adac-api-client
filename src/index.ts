d423fe7ba3bb17e3fe4beeedc25df3fbe119e0a5
kevinkosterr <koster.k2001@gmail.com>
2025-08-07 13:31:04 +0200

    fix: export types from root

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
 import ADAC from '@/client.ts'
 
 export default ADAC
+
+export type {
+  Action,
+  AdacUserConfig,
+  AdacErrorResponse,
+  DomainResult,
+  DomainPremiumResult,
+  SuggestionResult,
+  CategoriesResult,
+  Command,
+  CommandData,
+  Hints
+} from '@/resources/types.ts'
+export type {
+  DomainsBotLanguage,
+  DomainsBotOptions,
+  SidnOptions,
+  RnsOptions,
+  PrefixesSuffixesOptions,
+  NameSuggestionTLD,
+  NameSuggestionLanguage,
+  NameSuggestionOptions
+} from '@/resources/hints.types.ts'
