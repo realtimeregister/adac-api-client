1aca8fab5f9df3d50c898f0a53a6b82c53ccfcf5
kevinkosterr <koster.k2001@gmail.com>
2025-08-26 14:01:15 +0200

    chore: add adac playground for easier development

---
+++ apps/playground/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <input v-model="value" type="text">
+    <button @click="() => adac.getSuggestions(value)">Get Suggestions</button>
+    <div v-if="suggestions.length">
+      <h1>Suggestions</h1>
+      <ul>
+        <li v-for="suggestion in suggestions">{{ suggestion }}</li>
+      </ul>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import ADAC from '@/index.ts'
+
+const value = ref('')
+const suggestions = ref([])
+
+const adac = ADAC.initialize(import.meta.env.VITE_ADAC_API_KEY, {
+  apiHost: 'localhost:8082',
+  tldSetToken: import.meta.env.VITE_ADAC_TLD_SET_TOKEN,
+  debug: true,
+  disableSsl: true
+})
+
+adac.onSuggestion = (result) => {
+  suggestions.value.push(result.domain_name)
+}
+
+</script>
